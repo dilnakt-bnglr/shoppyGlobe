cc2acbfa1d6e931a093a0100222a479fc8492e06
dilnakt-bnglr <dilna.devaraj98@gmail.com>
2026-03-19 13:54:18 +0530

    added cartcount to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { BsCart3 } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Header() {
+  const cartCount = useSelector((store) => store.cart.cart.length);
   return (
     <div className="d-flex justify-content-between align-items-center bg-light px-4 py-2">
       <p className="text-capitalize fw-bolder fs-3 mb-0">ShoppyGlobe</p>
@@ -22,7 +24,11 @@ function Header() {
           <li className="position-relative">
             <Link to="/cart" className="text-decoration-none text-dark">
               <BsCart3 />
-              <span className="ms-1">6</span>
+              {cartCount > 0 ? (
+                <span className="ms-1 text-primary fw-bold">{cartCount}</span>
+              ) : (
+                ""
+              )}
             </Link>
           </li>
         </ul>
